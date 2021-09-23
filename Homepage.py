abe6d4aef730c7c4b4a9c8fa9e2cf3650e45e7d9
Advik-B <advik.b@gmail.com>
2021-09-23 14:27:48 +0530

    Small up of html content and icon display

--- Homepage.py
+++ Homepage.py
@@ -8,4 +8,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/favicon.ico')
+def favicon():
+    with open('favicon.ico', 'rb') as f:
+        return f.read()
+
+
 app.run('localhost', port='2380')
